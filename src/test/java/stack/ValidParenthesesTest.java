f519f152b79deb5c643ab37ded2ebf1ce4966d5f
SeolYoungKim <nasur4da@gmail.com>
2024-06-23 21:38:12 +0900

    feat: ValidParentheses - 책 풀이 - Map활용 (리트코드 20)

--- src/test/java/stack/ValidParenthesesTest.java
+++ src/test/java/stack/ValidParenthesesTest.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.ArrayDeque;
 import java.util.Deque;
+import java.util.Map;
 import org.junit.jupiter.api.Test;
 
 /**
@@ -11,13 +12,20 @@ import org.junit.jupiter.api.Test;
  */
 public class ValidParenthesesTest {
     public boolean isValid(String s) {
+        Map<Character, Character> parenthesesMap = Map.of(
+                ')', '(',
+                '}', '{',
+                ']', '['
+        );
+
         Deque<Character> stack = new ArrayDeque<>();
         for (char c : s.toCharArray()) {
-            Character peek = stack.peek();
-            if (peek != null && (peek == '(' && c == ')' || peek == '{' && c == '}' || peek == '[' && c == ']')) {
-                stack.pop();
-            } else {
+            // character가 열림 괄호인 경우 스택에 push
+            if (!parenthesesMap.containsKey(c)) {
                 stack.push(c);
+            // 닫힘 괄호인 경우, 스택이 비었거나 스택에서 pop한 것이 동일한 유형의 열림 괄호가 아니면 false
+            } else if (stack.isEmpty() || parenthesesMap.get(c) != stack.pop()) {
+                return false;
             }
         }
 

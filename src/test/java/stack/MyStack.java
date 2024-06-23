df1b028dd133f41cb0882f6c525865aaa43cf59c
SeolYoungKim <nasur4da@gmail.com>
2024-06-23 21:29:07 +0900

    feat: LinkedList로 Stack 구현

---
+++ src/test/java/stack/MyStack.java
@@ -0,0 +1,25 @@
+package stack;
+
+public class MyStack {
+    private static class Node {
+        int item;
+        Node next;
+
+        public Node(int item, Node next) {
+            this.item = item;
+            this.next = next;
+        }
+    }
+
+    private Node last;
+
+    public void push(int item) {
+        this.last = new Node(item, this.last);
+    }
+
+    public int pop() {
+        int item = this.last.item;
+        this.last = this.last.next;
+        return item;
+    }
+}
